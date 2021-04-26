2621a7b950fc9d2e6082f7056b0c5186de9a4fa4
Fabio Pereira da Silva <fabioegel@gmail.com>
2021-04-26 20:18:59 -0300

    Preparing to add callback to C_TEST_SETTER. Adding ignore function

--- src/main.c
+++ src/main.c
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
 
    //assert_false(0, "Erro bool", "Sucesso bool");
 
-   C_ASSERT_FALSE(0);
+   C_ASSERT_FALSE(0)
 
    C_ASSERT_TRUE(1, CTEST_SETTER(
       CTEST_TITLE("This is a title with value %d", 5),
@@ -34,7 +34,8 @@ int main(int argc, char **argv)
       CTEST_WARN("This is a WARN message"),
       CTEST_ON_ERROR("This is a message when error occurs"),
       CTEST_ON_SUCCESS("This is a message when SUCCESS occurs")
-   ));
+   ))
+
 /*
    C_ASSERT_TRUE(1, CTEST_SETTER(
       CTEST_TITLE("This is a title with value %d", 5),
@@ -48,32 +49,32 @@ NULL
    ));
 */
 
-   C_ASSERT_EQUAL_INT(-1, -1);
+   C_ASSERT_EQUAL_INT(-1, -1)
 
-   C_ASSERT_NOT_EQUAL_INT(11,12);
+   C_ASSERT_NOT_EQUAL_INT(11,12)
 
-   C_ASSERT_EQUAL_LONG_INT(3, 3);
-   C_ASSERT_NOT_EQUAL_LONG_INT(1, 0xf1);
+   C_ASSERT_EQUAL_LONG_INT(3, 3)
+   C_ASSERT_NOT_EQUAL_LONG_INT(1, 0xf1)
 
-   C_ASSERT_EQUAL_DOUBLE(2., 2.3-0.3, delta);
+   C_ASSERT_EQUAL_DOUBLE(2., 2.3-0.3, delta)
 
-   C_ASSERT_NOT_EQUAL_DOUBLE(5.6, 2.3-0.3, delta);
+   C_ASSERT_NOT_EQUAL_DOUBLE(5.6, 2.3-0.3, delta)
 
-   C_ASSERT_EQUAL_BYTE(vec1, vec2, sizeof(vec1));
+   C_ASSERT_EQUAL_BYTE(vec1, vec2, sizeof(vec1))
 
-   C_ASSERT_NOT_EQUAL_BYTE(vec1, vec3, sizeof(vec1), CTEST_SETTER(CTEST_INFO("Testando esse vetor"), CTEST_WARN("ALERTA: Ele deve ser diferente!!!")));
+   C_ASSERT_NOT_EQUAL_BYTE(vec1, vec3, sizeof(vec1), CTEST_SETTER(CTEST_INFO("Testando esse vetor"), CTEST_WARN("ALERTA: Ele deve ser diferente!!!")))
 
-   C_ASSERT_NULL(NULL);
+   C_ASSERT_NULL(NULL)
 
-   C_ASSERT_NOT_NULL(vec3);
+   C_ASSERT_NOT_NULL(vec3)
 
-   C_ASSERT_EQUAL_STRING(name1, name2);
+   C_ASSERT_EQUAL_STRING(name1, name2)
 
-   C_ASSERT_EQUAL_STRING_IGNORE_CASE(name3, name2);
+   C_ASSERT_EQUAL_STRING_IGNORE_CASE(name3, name2)
 
-   C_ASSERT_NOT_EQUAL_STRING(name1, name3);
+   C_ASSERT_NOT_EQUAL_STRING(name1, name3)
 
-   C_ASSERT_NOT_EQUAL_STRING_IGNORE_CASE(name3, "Albert Einstein");
+   C_ASSERT_NOT_EQUAL_STRING_IGNORE_CASE(name3, "Albert Einstein")
 
    end_tests();
 
